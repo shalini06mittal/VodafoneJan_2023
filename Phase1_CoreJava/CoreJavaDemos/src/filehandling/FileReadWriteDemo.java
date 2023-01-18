cc47c63b68c4ed92a99012e752aa0d0619fd51bf
Shalini Mittal <shalini06.mittal@gmail.com>
2023-01-18 18:26:20 +0530

    Day 8 files

---
+++ Phase1_CoreJava/CoreJavaDemos/src/filehandling/FileReadWriteDemo.java
@@ -0,0 +1,55 @@
+package filehandling;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileReadWriteDemo {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		String path = System.getProperty("user.dir");
+		File files = new File(path);
+		
+		for(File f:files.listFiles())
+		{
+			System.out.println(f);
+			System.out.println(f.isDirectory());
+		}
+		
+		File dir = new File("/Users/Shalini/Documents/data.numbers");
+		//dir.mkdir();
+		
+		File file = new File(dir, "test.txt");
+		//file.createNewFile();
+	
+		System.out.println("deleting");
+		System.out.println(dir.delete());
+		
+		/*
+		 * InputStream/OutputStream => bytes (media) hello
+		 * Reader/writer => textual files
+		 */
+		
+//		FileWriter writer = new FileWriter("demo.txt", true);
+//		writer.write("Hello now\n");
+//		writer.write("Welcome!!!\n");
+//		writer.close();
+		
+		FileReader reader = new FileReader("demo.txt");
+		BufferedReader bufferedReader = new BufferedReader(reader);
+		String line = "";
+		while( (line = bufferedReader.readLine() ) != null)
+		{
+			if(line.startsWith("Hello"))
+				System.out.println(line);
+		}
+		
+		bufferedReader.close();
+		reader.close();
+		
+	}
+}
