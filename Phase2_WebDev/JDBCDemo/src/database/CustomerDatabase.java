c44fa53936f5b967464366638edb18cfec1f2a0b
Shalini Mittal <shalini06.mittal@gmail.com>
2023-02-13 18:11:41 +0530

    mysql

---
+++ Phase2_WebDev/JDBCDemo/src/database/CustomerDatabase.java
@@ -0,0 +1,59 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import entity.Customer;
+
+public class CustomerDatabase {
+
+	// fetch all records -> select *
+	public List<Customer> getAllCustomers() throws SQLException
+	{
+		List<Customer> customers = new ArrayList<Customer>();
+
+		// select * from customer
+		//String sql = "select city, phone from customer";
+		String sql = "select * from customer";
+
+		// 1. DB connection 
+		Connection conn = DBConnection.dbConn();
+		//2. create the statememt
+		Statement stat = conn.createStatement();
+		// 3. execute the query
+		ResultSet rs = stat.executeQuery(sql);
+		while(rs.next()) {
+			Customer c1 = new Customer();
+			c1.setEmail(rs.getString(1));
+			c1.setPhone(rs.getString(2));
+			c1.setPassword(rs.getString(3));
+			c1.setCity(rs.getString(4));
+			c1.setIsmember(rs.getBoolean(5));
+			customers.add(c1);
+		}
+		return customers;
+	}
+	public boolean insertCustomer(Customer customer) throws SQLException
+	{
+		String sql = "insert into customer values('" + customer.getEmail()+
+				"','"+ customer.getPhone()+  "','"+customer.getPassword() +
+				"','"+customer.getCity()+"',"+customer.isIsmember()+")";
+		System.out.println(sql);
+		// 1. DB connection 
+		Connection conn = DBConnection.dbConn();
+		//2. create the statememt
+		Statement stat = conn.createStatement();
+		try {
+		stat.executeUpdate(sql);
+		}catch(Exception e)
+		{
+			System.out.println("error "+e.getMessage());
+			return false;
+		}
+		return true;
+	}
+}
---
+++ Phase2_WebDev/JDBCDemo/src/database/TestDB.java
@@ -0,0 +1,20 @@
+package database;
+
+import java.sql.SQLException;
+
+import entity.Customer;
+
+public class TestDB {
+
+	public static void main(String[] args) throws SQLException {
+		//DBConnection.dbConn();
+		CustomerDatabase db = new CustomerDatabase();
+//		for(Customer c: db.getAllCustomers())
+//			System.out.println(c);
+		
+		boolean insertSuccess = db.insertCustomer(new Customer("ge@g.c","9999999999","ge","Chicago",false));
+		System.out.println(insertSuccess);
+
+	}
+
+}
