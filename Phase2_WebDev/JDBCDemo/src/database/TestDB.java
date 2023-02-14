ce2197f1220f81309e115c523228019d8f82b609
Shalini Mittal <shalini06.mittal@gmail.com>
2023-02-14 15:06:00 +0530

    mysql

--- Phase2_WebDev/JDBCDemo/src/database/TestDB.java
+++ Phase2_WebDev/JDBCDemo/src/database/TestDB.java
@@ -9,13 +9,30 @@ public class TestDB {
 	public static void main(String[] args) throws SQLException {
 		//DBConnection.dbConn();
 		CustomerDatabase db = new CustomerDatabase();
-		for(Customer c: db.getAllCustomers())
-			System.out.println(c);
-		
-		db.getAllCities();
+//		for(Customer c: db.getAllCustomers())
+//			System.out.println(c);
+//		
+//		db.getAllCities();
 		
 //		boolean insertSuccess = db.insertCustomer(new Customer("ge@g.c","9999999999","ge","Chicago",false));
 //		System.out.println(insertSuccess);
+
+//		boolean insertSuccess = db.insertCustomerUsingPrepareStatement(new Customer("ge@g.c","9999999999","ge","Chicago",false));
+//		System.out.println(insertSuccess);
+		
+		Customer c1 = new Customer();
+		c1.setEmail("ge@g.c");
+		c1.setCity("New York");
+		c1.setPhone("9898989898");
+		//System.out.println(db.updateCustomerByEmail(c1.getEmail(), c1.getPhone(), c1.getCity()));
+		
+//		for(Customer c: db.getAllCustomers())
+//			System.out.println(c);
+		
+		System.out.println(db.getCustomerByEmail("cd@g.c"));
+		
+		System.out.println(db.deleteCustomerByEmail("xyz@g.c"));
+		
 
 	}
 
