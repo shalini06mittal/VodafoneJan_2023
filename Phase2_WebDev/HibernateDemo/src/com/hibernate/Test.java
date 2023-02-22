42dee0e6e957f98f76f9ff553d138d7f635b8c27
Shalini Mittal <shalini06.mittal@gmail.com>
2023-02-22 17:51:02 +0530

    hibernate basic

--- Phase2_WebDev/HibernateDemo/src/com/hibernate/Test.java
+++ Phase2_WebDev/HibernateDemo/src/com/hibernate/Test.java
@@ -2,6 +2,10 @@ package com.hibernate;
 
 
 
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaQuery;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -15,15 +19,17 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("config loaded");
 		Book b1 = new Book();
-//		b1.setAuthor("Shalini Mittal");
-//		b1.setBooktitle("HTML5");
-//		b1.setPrice(550);
-//		b1.setYear(2004);
-		b1.setBid(2);
-		//System.out.println(saveBook(b1));
+		b1.setAuthor("Shalini Mittal");
+		b1.setBooktitle("HTML5");
+		b1.setPrice(550);
+		b1.setYear(2004);
+		//b1.setBid(2);
+		System.out.println(saveBook(b1));
 		//System.out.println(getBookById(2));
 		//updateBook(b1);
-		deleteBook(b1);
+		//deleteBook(b1);
+		for(Book b:getAllBooks())
+			System.out.println(b);
 	}
 	
 	public static Integer saveBook(Book book) {
@@ -58,6 +64,16 @@ public class Test {
 		session.close();
 
 	}
+	
+	public static List<Book> getAllBooks()
+	{
+		Session session = factory.openSession();
+		CriteriaQuery<Book> cq = session.getCriteriaBuilder().createQuery(Book.class);
+		cq.from(Book.class);
+		List<Book> booklist = session.createQuery(cq).getResultList();
+		return booklist;
+	}
+
 
 }
 
