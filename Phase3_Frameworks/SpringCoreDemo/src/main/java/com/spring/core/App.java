ddaec3c13616c992edb3c228bdec02fd4066f7c3
Shalini Mittal <shalini06.mittal@gmail.com>
2023-03-20 16:16:44 +0530

    phase 3 day1 spring core

--- Phase3_Frameworks/SpringCoreDemo/src/main/java/com/spring/core/App.java
+++ Phase3_Frameworks/SpringCoreDemo/src/main/java/com/spring/core/App.java
@@ -3,6 +3,8 @@ package com.spring.core;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.spring.core.entity.Author;
+
 /**
  *All the beans or java classes configured in spring.xml will be 
  *loaded and instantiated by spring
@@ -14,5 +16,10 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Author author = context.getBean(Author.class);
+        System.out.println(author);
+        
+//        Author a1 = new Author(1, "", "");
+//        a1.setAid(0);
     }
 }
