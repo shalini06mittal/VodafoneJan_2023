ec45cb70ca4ccb5874d5220d5d123d2661cc7cbb
Shalini Mittal <shalini06.mittal@gmail.com>
2023-05-15 15:02:35 +0530

    Javascript

---
+++ Phase4_WedDevFramework/javascript/basic.js
@@ -0,0 +1,38 @@
+ /*
+         * data types
+         number : int and float
+        string : '' ""
+        boolean
+        null
+        undefined
+        Nan
+        variables : dynamically typed language
+        either by using var or let
+        Preferable is using let => restrictive , nuisance
+        ES6 => ecmascript
+         */
+        //  let y = 10;
+        //  console.log(y) // variable hoisting => declaration and not the initialization
+         
+        //  let x = 10;
+        //  console.log(x, typeof(x));
+        //  x = "hello";
+        //  console.log(x, typeof(x));
+        //  x = true;
+        //  console.log(x, typeof(x));
+        //  x = null;
+        //  console.log(x, typeof(x));
+        //  x = undefined;
+        //  console.log(x, typeof(x));
+        //  x = NaN;
+        //  console.log(x, typeof(x));
+         // unary => ++ --
+         // arithmetic + - * / % **
+         console.log(Math.floor(137/60));
+         console.log(137%60);
+         console.log(5**2);
+         // relational < > <= >= == !=
+         // ===
+         console.log(1 == '1'); // only checks the value
+         console.log(1 === '1'); // type and then the value
+         // logical && ||
