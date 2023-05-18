ed0b4c62f4d6633db33b7ea9913adb7dd0bad6b7
Shalini Mittal <shalini06.mittal@gmail.com>
2023-05-18 16:25:42 +0530

    Javascript

--- Phase4_WedDevFramework/reactjs/reactapp/src/components/ClockClass.js
+++ Phase4_WedDevFramework/reactjs/reactapp/src/components/ClockClass.js
@@ -5,7 +5,8 @@ export default class ClockClass extends Component {
     constructor(props) {
       super(props)
       this.state = {
-         currdate: new Date().toLocaleString()
+         currdate: new Date().toLocaleString(),
+         message:'Some message'
       }
     }
     // updateTime = ()=>
@@ -20,12 +21,14 @@ export default class ClockClass extends Component {
         console.log(this)
         console.log(`before ${this.state.currdate}`);
         //this.state.currdate = new Date().toLocaleString();
-        this.setState({currdate : new Date().toLocaleString()})
+        this.setState({currdate : new Date().toLocaleString()
+            ,message:'Time Updated' })
         console.log(`after ${this.state.currdate}`);
     }
   render() {
     return (
       <div>
+        <p>{this.state.message}</p>
         <p>{this.state.currdate}</p>
         {/* <div><button onClick={this.updateTime}>Show current time</button></div> */}
         <div><button onClick={()=>this.updateTime()}>Show current time</button></div>
--- Phase4_WedDevFramework/reactjs/reactapp/src/components/ClockFunc.js
+++ Phase4_WedDevFramework/reactjs/reactapp/src/components/ClockFunc.js
@@ -3,17 +3,19 @@ import React, { useState } from 'react'
 export default function ClockFunc() {
 
     const [currdate, setCurrDate] = useState(new Date().toLocaleString())
-
+    const [counter, setcounter] = useState(0)
     const updateTime = ()=>
     {
          console.log(`before ${currdate}`);
          //this.state.currdate = new Date().toLocaleString();
          setCurrDate(new Date().toLocaleString())
+         setcounter(counter+1)
          console.log(`after ${currdate}`);
     }
   return (
     <div>
         <h1>Clock function</h1>
+        <p>{counter}</p>
         <p>{currdate}</p>
 
         
