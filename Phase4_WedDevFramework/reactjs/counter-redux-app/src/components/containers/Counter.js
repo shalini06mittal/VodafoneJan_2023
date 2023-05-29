25ee36806e7959cc7dc104082ffc1d6ce0be9707
Shalini Mittal <shalini06.mittal@gmail.com>
2023-05-29 16:31:20 +0530

    reduc

--- Phase4_WedDevFramework/reactjs/counter-redux-app/src/components/containers/Counter.js
+++ Phase4_WedDevFramework/reactjs/counter-redux-app/src/components/containers/Counter.js
@@ -1,33 +1,35 @@
 import React, { Component } from 'react'
 import CounterControl from '../output/CounterControl'
 import CounterOutput from '../output/CounterOutput'
+import { connect } from 'react-redux'
+import { increment } from '../../reducers/counterreducer'
 
 class Counter extends Component {
-    state={
-        count:0
-    }
-    handleCounterChange = (action, value)=>{
-        switch(action){
-            case 'inc':
-                this.setState({count:this.state.count+1});
-                break;
-            case 'dec':
-                    this.setState({count:this.state.count-1});
-                    break;
-            case 'add':
-                        this.setState({count:this.state.count+value});
-                        break;
-            case 'sub':
-                this.setState({count:this.state.count-value});
-                break;
-        }
-    }
+    // state={
+    //     count:0
+    // }
+     handleCounterChange = (action, value)=>{
+    //     switch(action){
+    //         case 'inc':
+    //             this.setState({count:this.state.count+1});
+    //             break;
+    //         case 'dec':
+    //                 this.setState({count:this.state.count-1});
+    //                 break;
+    //         case 'add':
+    //                     this.setState({count:this.state.count+value});
+    //                     break;
+    //         case 'sub':
+    //             this.setState({count:this.state.count-value});
+    //             break;
+    //     }
+     }
   render() {
       console.log(this.props)
     return (
       <div>
-          <CounterOutput value={this.state.count}/>
-          <CounterControl label='Increment' clicked={()=>this.handleCounterChange('inc')}/>
+          <CounterOutput value={this.props.count}/>
+          <CounterControl label='Increment' clicked={()=>this.props.onIncrement()}/>
           <CounterControl label='Decrement' clicked={()=>this.handleCounterChange('dec')}/>
           <CounterControl label='Add 5' clicked={()=>this.handleCounterChange('add',5)}/>
           <CounterControl label='Sub 3' clicked={()=>this.handleCounterChange('sub',3)}/>
@@ -35,4 +37,13 @@ class Counter extends Component {
     )
   }
 }
-export default Counter;
+
+const mapStateOfStoreToProps = (state)=>{
+  return {count : state.counterreducer.count}
+}
+const mapDispatchActionsToProps = (dispatch)=>{
+  return {  
+    onIncrement : () => dispatch(increment())
+  }
+}
+export default connect(mapStateOfStoreToProps, mapDispatchActionsToProps)(Counter);
--- Phase4_WedDevFramework/reactjs/counter-redux-app/src/reducers/counterreducer.js
+++ Phase4_WedDevFramework/reactjs/counter-redux-app/src/reducers/counterreducer.js
@@ -1,6 +1,6 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-const initialState = { count : 0}
+const initialState = { count : 10}
 
 const counter = createSlice({
     name:'counter',
