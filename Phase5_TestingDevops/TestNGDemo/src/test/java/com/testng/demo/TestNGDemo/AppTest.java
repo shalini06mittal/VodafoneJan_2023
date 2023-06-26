fcd11d62174930d76a9e904bdf5bf08f0c7fea63
Shalini Mittal <shalini06.mittal@gmail.com>
2023-06-26 16:07:05 +0530

    testng

--- Phase5_TestingDevops/TestNGDemo/src/test/java/com/testng/demo/TestNGDemo/AppTest.java
+++ Phase5_TestingDevops/TestNGDemo/src/test/java/com/testng/demo/TestNGDemo/AppTest.java
@@ -1,8 +1,7 @@
 package com.testng.demo.TestNGDemo;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import static org.testng.AssertJUnit.assertTrue;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
--- Phase5_TestingDevops/TestNGDemo/src/test/java/com/testng/demo/TestNGDemo/FirstTestCase.java
+++ Phase5_TestingDevops/TestNGDemo/src/test/java/com/testng/demo/TestNGDemo/FirstTestCase.java
@@ -1,5 +1,6 @@
 package com.testng.demo.TestNGDemo;
 
+import org.testng.annotations.Test;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
@@ -10,21 +11,21 @@ import org.testng.annotations.Test;
 
 public class FirstTestCase {
 	
-	@BeforeMethod
-	public void beforeMethod()
-	{
-		System.out.println("before method");
-	}
-	@BeforeTest
-	public void beforeTest()
-	{
-		System.out.println("before test");
-	}
-	@BeforeClass
-	public void beforeClass()
-	{
-		System.out.println("before class");
-	}
+//	@BeforeMethod
+//	public void beforeMethod()
+//	{
+//		System.out.println("before method");
+//	}
+//	@BeforeTest
+//	public void beforeTest()
+//	{
+//		System.out.println("before test");
+//	}
+//	@BeforeClass
+//	public void beforeClass()
+//	{
+//		System.out.println("before class");
+//	}
 	@Test
 	public void test1()
 	{
@@ -35,20 +36,20 @@ public class FirstTestCase {
 	{
 		System.out.println("Test 2");
 	}
-	@AfterMethod
-	public void afterMethod()
-	{
-		System.out.println("after method");
-	}
-	@AfterTest
-	public void afterTest()
-	{
-		System.out.println("after test");
-	}
-	@AfterClass
-	public void afterClass()
-	{
-		System.out.println("after class");
-	}
+//	@AfterMethod
+//	public void afterMethod()
+//	{
+//		System.out.println("after method");
+//	}
+//	@AfterTest
+//	public void afterTest()
+//	{
+//		System.out.println("after test");
+//	}
+//	@AfterClass
+//	public void afterClass()
+//	{
+//		System.out.println("after class");
+//	}
 
 }
