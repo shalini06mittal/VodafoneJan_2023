be91afc7c43a5cf4b6fdcb21409a50b00f239b31
Shalini Mittal <shalini06.mittal@gmail.com>
2023-06-26 14:47:29 +0530

    testng

---
+++ Phase5_TestingDevops/TestNGDemo/src/test/java/com/testng/demo/TestNGDemo/AppTest.java
@@ -0,0 +1,20 @@
+package com.testng.demo.TestNGDemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
